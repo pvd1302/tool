02aedf00361e266b01c2ca2c6a7c8c262ecd62ef
pvd1302 <pvduc.ns@gmail.com>
2023-09-15 09:00:32 +0700

    update
    nhập link từ file + retry request

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import copy
 import os
 import uuid
 import re
+import time
+from dotenv import load_dotenv
 
 import requests
+from requests.adapters import HTTPAdapter, Retry
 from requests.auth import HTTPBasicAuth
 from selenium import webdriver
 
@@ -15,13 +18,23 @@ from selenium.webdriver.common.by import By
 import pdfkit
 import shutil
 
+# -*- coding: utf-8 -*-
+load_dotenv('config.env')
+
+session = requests.Session()
+reties = Retry(total=3,
+               backoff_factor=0.1,
+               status_forcelist=[500, 502, 503, 504, 400, 401, 404])
+
+session.mount("http://", requests.adapters.HTTPAdapter(max_retries=reties))
+session.mount("https://", requests.adapters.HTTPAdapter(max_retries=reties))
+
 
 def downloadFile(link, file_name):
-
     path_download = "download"
     os.makedirs(path_download, exist_ok=True)
     basic = HTTPBasicAuth('mobio', 'mobio!@#456')
-    resp = requests.get(link, auth=basic)
+    resp = session.get(link, auth=basic)
     r = os.path.join(path_download, file_name)
 
     with open(r, 'w', encoding="UTF-8") as f:
@@ -32,21 +45,21 @@ def downloadFile(link, file_name):
     return r
 
 
-
 def replace_link_css_or_image(link):
     link = link.replace('assets', domain + '/assets')
     link = link.replace('vendor', domain + '/vendor')
     link = link.replace('css/', domain + '/css/')
     link = link.replace('img/', domain + '/img/')
+
     return link
 
 
-def replace_link_script(link):
-    link = link.replace('assets', domain + '/assets').replace("././", "").replace("./", "")
-    link = link.replace('vendor', domain + '/vendor').replace("././", "").replace("./", "")
-    link = link.replace('locales', domain + '/locales').replace("././", "").replace("./", "")
-    link = link.replace('utils', domain + '/utils').replace("././", "").replace("./", "")
-    return link
+# def replace_link_script(link):
+#     link = link.replace('assets', domain + '/assets').replace("././", "").replace("./", "")
+#     link = link.replace('vendor', domain + '/vendor').replace("././", "").replace("./", "")
+#     link = link.replace('locales', domain + '/locales').replace("././", "").replace("./", "")
+#     link = link.replace('utils', domain + '/utils').replace("././", "").replace("./", "")
+#     return link
 
 
 def convert_filename(filename):
@@ -59,7 +72,6 @@ def convert_filename(filename):
 
 
 if __name__ == '__main__':
-    url = input("Nhap url : ")
     username = "mobio"
     password = "mobio!@#456"
     replace_pass = {'!': '%21',
@@ -85,90 +97,97 @@ if __name__ == '__main__':
                     '@': '%40'}
     for key, value in replace_pass.items():
         password = password.replace(key, value)
-    nameDelete = url.split('#api-')
-    first = ''
-    apiName = url.split('/#')
-    divIDApiName = apiName[1]
-    domain = apiName[0]
-    split_url = url.split('https://')
-    parse_url = "https://" + username + ":" + password + "@" + split_url[1]
-    edge_options = webdriver.ChromeOptions()
-    edge_options.add_argument("--headless")
-    driver = webdriver.Chrome(edge_options)
-    driver.get(parse_url)
+    PATH_WKHTMLTOPDF = os.getenv('PATH_WKHTMLTOPDF')
+    path_wkhtmltopdf = r'{}'.format(PATH_WKHTMLTOPDF)
+    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+    FILE_LINK_EXPORT = os.getenv('FILE_LINK_EXPORT')
 
-    timeout = 30
-    count = 3
-    body = None
-    while count > 0:
-        print("Start find :: ", count)
-        element_present = EC.presence_of_element_located((By.ID, apiName[1]))
-        WebDriverWait(driver, timeout).until(element_present)
+    repos = []
+    with open(FILE_LINK_EXPORT) as infile:
+        for line in infile:
+            line = line.strip()
+            if line:
+                repos.append(line)
+    for url in repos:
+        # if not url.strip():
+        #     continue
+        print(url)
+
+        nameDelete = url.split('#api-')
+        first = ''
+        apiName = url.split('/#')
+        divIDApiName = apiName[1]
+        domain = apiName[0]
+        split_url = url.split('https://')
+        parse_url = "https://" + username + ":" + password + "@" + split_url[1]
+        edge_options = webdriver.ChromeOptions()
+        edge_options.add_argument("--headless")
+        driver = webdriver.Chrome(edge_options)
+        driver.get(parse_url)
         html = driver.page_source
 
-        soup = bs(html, 'html.parser')
-        header_example = soup.find_all("a", href=re.compile("#header-examples-" + nameDelete[1]))
-        a_error_example = soup.find_all("a", href=re.compile("#error-examples-" + nameDelete[1]))
-        div_error_example = soup.find_all("div", id=lambda id: id and "error-examples" in id)
+        timeout = 100
+        count = 3
+        body = None
+        while count > 0:
+            print("Start find :: ", count)
+            element_present = EC.presence_of_element_located((By.ID, apiName[1]))
+            WebDriverWait(driver, timeout).until(element_present)
+            html = driver.page_source
 
-        if header_example and len(header_example) > 1:
-            header_example.pop(0)
-        for e in header_example:
-            e.extract()
-        for e3 in a_error_example:
-            e3.extract()
-        for e4 in div_error_example:
-            e4.extract()
-        body = soup.find('div', {'id': divIDApiName})
-        if body:
-            break
+            soup = bs(html, 'html.parser')
+            header_example = soup.find_all("a", href=re.compile("#header-examples-" + nameDelete[1]))
+            a_error_example = soup.find_all("a", href=re.compile("#error-examples-" + nameDelete[1]))
+            div_error_example = soup.find_all("div", id=lambda id: id and "error-examples" in id)
 
-        count -= 1
-    #
-    print("Start download link css/image")
-    for a in soup.find_all('link'):
-        a_href = a['href']
-        a_href = replace_link_css_or_image(a_href)
-        if a_href.startswith(domain):
-            file_new = a_href.replace(domain, "")
-            a_href = downloadFile(a_href, convert_filename(file_new))
-        a['href'] = a_href
+            if header_example and len(header_example) > 1:
+                header_example.pop(0)
+            for e in header_example:
+                e.extract()
+            for e3 in a_error_example:
+                e3.extract()
+            for e4 in div_error_example:
+                e4.extract()
+            body = soup.find('div', {'id': divIDApiName})
+            if body:
+                break
 
-    print("Start download link script")
-    for script in soup.find_all('script'):
-        src = script.get('src')
-        if not src:
-            continue
-        src = replace_link_script(src)
-        for i in ["main.js", "api_data.js", "api_project.js", "utils.js"]:
-            if src.startswith(i):
-                src = domain + "/" + src.replace("./", "/")
-        if src.startswith(domain):
-            file_new = src.replace(domain, "")
-            src = downloadFile(src, convert_filename(file_new))
-        data_main = script.get("data-main")
+            count -= 1
+        #
+        print("Start download link css/image")
+        # start = time.time()
 
-        if data_main:
-            script["data-main"] = domain + "/" + data_main
-        script['src'] = src
+        for a in soup.find_all('link'):
+            a_href = a['href']
+            a_href = replace_link_css_or_image(a_href)
+            if a_href.startswith(domain):
+                file_new = a_href.replace(domain, "")
+                if os.path.isfile(a_href):
+                    print("File exist")
+                else:
+
+                    a_href = downloadFile(a_href, convert_filename(file_new))
+            a['href'] = a_href
         # end = time.time()
-        # time_run = end - start
-        # print(src, time_run)
+        # run_time = end - start
+        # print(a_href, convert_filename(file_new), run_time)
+        print("Start convert to html")
+        head = soup.find('head')
 
-    print("Start convert to html")
-    head = soup.find('head')
+        result = str(head) + str(body)
 
-    result = str(head) + str(body)
-
-    outFilePathHtml = divIDApiName + ".html"
-    outFilePathPdf = divIDApiName + ".pdf"
-    print("Start save file html")
-    with open(outFilePathHtml, "w", encoding="UTF-8") as f:
-        f.write(result)
-
-    print("Start config export file pdf")
-    config = pdfkit.configuration()
-    pdfkit.from_file(outFilePathHtml, outFilePathPdf, options={"enable-local-file-access": ""}, configuration=config)
-    print("Export done")
-    shutil.rmtree("download")
-    os.remove(outFilePathHtml)
+        outFilePathHtml = divIDApiName + ".html"
+        outFilePathPdf = divIDApiName + ".pdf"
+        print("Start save file html")
+        with open(outFilePathHtml, "w", encoding="UTF-8") as f:
+            f.write(result)
+        #
+        print("Start config export file pdf")
+        # path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+        try:
+            pdfkit.from_file(outFilePathHtml, outFilePathPdf, options={"enable-local-file-access": ""},
+                             configuration=config)
+        except:
+            print("Export done")
+        os.remove(outFilePathHtml)
+        shutil.rmtree("download")
